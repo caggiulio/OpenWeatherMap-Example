b49a5a697b47110d07f63cbfa3d461e1370781e1
nunzio giulio caggegi <nunziogiuliocaggegi@gmail.com>
2017-11-20 11:29:57 +0100

    Initial Commit

---
+++ weather_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weather_test
+//
+//  Created by nunzio giulio caggegi on 20/11/17.
+//  Copyright © 2017 nunzio giulio caggegi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
